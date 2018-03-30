08c210b358eac3b1bf0ebc970ee6fde8c78192d5
Travis Overby <travisoverby@gmail.com>
2018-03-30 13:52:41 -0700

    Cleanup idk why these aren't saving

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+global.__basePath = __dirname;
+
 const express = require('express');  
 const bodyParser = require('body-parser');
 const path = require('path')
@@ -17,11 +19,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // include routes
-const routes = require('./routes/index');
+const routes = require(path.join(__dirname, 'routes', 'index'));
 app.use('/', routes);
 
 app.use((req, res, next) => {
-  var err = new Error('WHOOPS!');
+  var err = new Error('WHOOPS');
   err.status = 404;
   next(err);
 });
