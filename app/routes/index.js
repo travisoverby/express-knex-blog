9c70b39db1367fcf5c5e81ad810dc5de1f48e309
Travis Overby <travisoverby@gmail.com>
2018-03-30 13:51:28 -0700

    Cleanup

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,36 +3,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'null', name: 'null' });
-});
+console.log(__basePath);
+console.log(__dirname);
+console.log(__dirname.split('/').pop());
+process.exit();
 
-router.get('/profile', (req, res, next) => {
-  res.render('profile', { title: 'null', name: 'null' });
+router.get('/', (req, res, next) => {
+  res.render('index');
 });
 
 router.get('/about', (req, res, next) => {
-  res.render('about', { title: 'null', name: 'null' });
-});
-
-router.get('/register', (req, res, next) => {
-  res.render('register', { title: 'null', name: 'null' });
-});
-
-router.get('/contact', (req, res, next) => {
-  res.render('contact', { title: 'null', name: 'null' });
-});
-
-router.get('/admin', (req, res, next) => {
-  res.render('admin', { title: 'null', name: 'null' });
-});
-
-router.get('/secret', (req, res, next) => {
-  res.render('secret', { title: 'null', name: 'null' });
-});
-
-router.get('/login', (req, res, next) => {
-  res.render('login', { title: 'null', name: 'null' });
+  res.render('about');
 });
 
 module.exports = router;
